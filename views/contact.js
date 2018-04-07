b5470de9cc1385715e61ba41f50708df9c0f4173
Seth Thompson <s3th.thompson@gmail.com>
2018-04-07 16:10:38 -0400

    add markdown support

--- views/contact.js
+++ views/contact.js
@@ -10,9 +10,21 @@ module.exports = view
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
+  var contact = state.site.pages.contact
+
   return html`
     <body class="ff-sans">
       ${header()}
+      <div class="c12 x xjb px2 pt5 pb1">
+        <div class="c4">
+          <div class="c3 psf mt5 mb1 t0 b0 l0 pl2" style="border-right: 1px solid #e0e0e0">
+          </div>
+        </div>
+        <div class="c4">
+        </div>
+        <div class="c4">
+        </div>
+      </div>
     </body>
   `
 }
--- views/sponsors.js
+++ views/sponsors.js
@@ -1,4 +1,5 @@
 var html = require('choo/html')
+var raw = require('choo/html/raw')
 var css = require('sheetify')
 
 var header = require('../elements/header')
@@ -10,9 +11,22 @@ module.exports = view
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
+  var sponsors = state.site.pages.sponsors
+
   return html`
     <body class="ff-sans">
       ${header()}
+      <div class="c12 x xjb px2 pt5 pb1">
+        <div class="c4">
+          <div class="c3 psf mt5 mb1 t0 b0 l0 pl2" style="border-right: 1px solid #e0e0e0">
+            ${raw(sponsors.html)}
+          </div>
+        </div>
+        <div class="c4">
+        </div>
+        <div class="c4">
+        </div>
+      </div>
     </body>
   `
 }
