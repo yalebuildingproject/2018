49bb99780609bd8a0d9533144565b0de560904f2
Seth Thompson <s3th.thompson@gmail.com>
2018-04-09 15:03:03 -0400

    hide scrollbar

--- elements/list-container.js
+++ elements/list-container.js
@@ -1,4 +1,11 @@
 var html = require('choo/html')
+var css = require('sheetify')
+
+var hidescroll = css`
+  :host::-webkit-scrollbar {
+    display: none;
+}
+`
 
 module.exports = layout
 
@@ -7,7 +14,7 @@ function layout (col1, col2) {
     <div class="c3 pr1-5 br1-lightgray">
       ${col1}
     </div>
-    <div class="c5 oa px1-5">
+    <div class="c5 container oa px1-5 undo-container ${hidescroll}">
       ${col2}
     </div>
     <div class="c4 pl1-5 bl1-lightgray">
