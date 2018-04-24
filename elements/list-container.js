8490c5fa62046bb668a08bda5d0d532a7a2dfa0f
Seth Thompson <s3th.thompson@gmail.com>
2018-04-24 12:12:08 -0400

    fewer scrollbars

--- elements/list-container.js
+++ elements/list-container.js
@@ -16,10 +16,10 @@ function layout (col1, col2, col3) {
     </div>` : ''
 
   return html`<div class="container pb1 x vh100">
-    <div sm="db" md="c3" class="c4 pr1-5 oys ${scroll} br1-lightgray dn">
+    <div sm="db" md="c3" class="c4 pr1-5 oa ${scroll} br1-lightgray dn">
       ${col1}
     </div>
-    <div sm="c8 px1-5" md="${three ? 'c5' : 'c9'}" class="c12 container oys  ${scroll} undo-container undo-bottom hide-scroll">
+    <div sm="c8 px1-5" md="${three ? 'c5' : 'c9'}" class="c12 container oa  ${scroll} undo-container undo-bottom hide-scroll">
       ${col2}
     </div>
     ${container3}
